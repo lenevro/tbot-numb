8c1f6476c0e2a0134cce8546879a583e90ea43c3
Zaur <barzarik@gmail.com>
2017-08-17 00:59:33 +0400

    Fix tabs settings

--- app/currency.js
+++ app/currency.js
@@ -15,16 +15,16 @@ bot.onText(/\/cc ([0-9]+) ([a-z]+) to ([a-z]+) *(.+[^a-z]+)*/i, (msg, match) => 
       let resVal;
 
       try {
-  			resVal = JSON.parse(body);
+        resVal = JSON.parse(body);
 
-  			if (resVal.error || !resVal.rates[match[3].toUpperCase()]) {
-			    throw new SyntaxError("Incorrect value");
-			  }
-  		} catch (e) {
-  			bot.sendMessage(userId, "Incorrect value");
-  			
-  			return;
-  		}
+        if (resVal.error || !resVal.rates[match[3].toUpperCase()]) {
+          throw new SyntaxError("Incorrect value");
+        }
+      } catch (e) {
+        bot.sendMessage(userId, "Incorrect value");
+
+        return;
+      }
 
       result = match[1] * resVal.rates[match[3].toUpperCase()];
 
@@ -34,43 +34,43 @@ bot.onText(/\/cc ([0-9]+) ([a-z]+) to ([a-z]+) *(.+[^a-z]+)*/i, (msg, match) => 
 
 bot.onText(/\/cc ([a-z]+) *(.+[^a-z]+)*/i, (msg, match) => {
     const userId = msg.from.id;
-        
+				
     let getDate = match[2] ? match[2].replace(/for /, "") : 'latest',
         result;
 
     //console.log(match);
 
     request('http://api.fixer.io/' + getDate + '?base=' + match[1].toUpperCase(), (error, response, body) => {
-  		let resVal,
-  				topVal;
+      let resVal,
+          topVal;
 
-  		try {
-  			resVal = JSON.parse(body);
+      try {
+        resVal = JSON.parse(body);
 
-  			if (resVal.error) {
-			    throw new SyntaxError("Incorrect value");
-			  }
-  		} catch (e) {
-  			bot.sendMessage(userId, "Incorrect value");
-  			
-  			return;
-  		}
+        if (resVal.error) {
+          throw new SyntaxError("Incorrect value");
+        }
+      } catch (e) {
+        bot.sendMessage(userId, "Incorrect value");
+
+        return;
+      }
 
       function round(key) {
-      	return Math.round(resVal.rates[key] * 100) / 100;
+        return Math.round(resVal.rates[key] * 100) / 100;
       }
 
       topVal = [
-      	"RUB: " + round("RUB"),
-				"EUR: " + round("EUR"),
-				"USD: " + round("USD")
-			];
+        "RUB: " + round("RUB"),
+        "EUR: " + round("EUR"),
+        "USD: " + round("USD")
+      ];
 
-			topVal.forEach((item, i) => {
-				if (~item.indexOf(match[1].toUpperCase())) topVal.splice(i, 1);
-			});
+      topVal.forEach((item, i) => {
+        if (~item.indexOf(match[1].toUpperCase())) topVal.splice(i, 1);
+      });
 
-			result = topVal.join("\n");
+      result = topVal.join("\n");
 
       bot.sendMessage(userId, result);
     });
