deaa8a6993438042af89d035cf753940f41d9c66
Zaur <barzarik@gmail.com>
2017-09-05 15:19:25 +0400

    Name of the currency at the end & Regex optimization

--- app/percentage.js
+++ app/percentage.js
@@ -4,36 +4,44 @@ const bot = require('./bot');
 
 function perEx(reg, res) {
   bot.onText(reg, (msg, match) => {
-    bot.sendMessage(msg.from.id, res(match));
+    if (clearNum(match[2])) bot.sendMessage(msg.from.id, res(match));
   });
+}
+
+function clearNum(match) {
+  return match.match(/([\d,.])/g) ? match.match(/([\d,.])/g).join("").replace(/\,/g, ".") : "";
+}
+
+function curreNum(match) {
+  return match.match(/[^0-9,.\s]/g) ? match.match(/[^0-9,.\s]/g).join("") : "";
 }
 
 /* Percentage value: 20% of $10 */
 
-perEx(/\/per +([0-9]+)% of ([^a-z]+)/i, (match) => {
-  return (match[1] * match[2].match(/\d+/)[0]) / 100;
+perEx(/\/per +([0-9,.\s]+)%\s+of\s+(.+)/i, (match) => {
+  return (clearNum(match[1]) * clearNum(match[2])) / 100 + " " + curreNum(match[2]);
 });
 
 /* Adding percentage: 5% on $30 */
 
-perEx(/\/per +([0-9]+)% on ([^a-z]+)/i, (match) => {
-  return (match[2].match(/\d+/)[0] / 100) * (+match[1] + 100);
+perEx(/\/per +([0-9,.\s]+)%\s+on\s+(.+)/i, (match) => {
+  return (clearNum(match[2]) / 100) * (+clearNum(match[1]) + 100) + " " + curreNum(match[2]);
 });
 
 /* Substracting percentage: 6% off 40 EUR */
 
-perEx(/\/per +([0-9]+)% off ([^a-z]+)/i, (match) => {
-  return (match[2].match(/\d+/)[0] / 100) * -(+match[1] - 100);
+perEx(/\/per +([0-9,.\s]+)%\s+off\s+(.+)/i, (match) => {
+  return (clearNum(match[2]) / 100) * -(+clearNum(match[1]) - 100) + " " + curreNum(match[2]);
 });
 
 /* Percentage value of one value relative to another: $50 as a % of $100 */
 
-perEx(/\/per +([^a-z]+) as a % of ([^a-z]+)/i, (match) => {
-  return match[1].match(/\d+/)[0] / (match[2].match(/\d+/)[0] / 100);
+perEx(/\/per +([^a-z]+)\s+as\s+a\s+%\s+of\s+([^a-z]+)/i, (match) => {
+  return clearNum(match[1]) / (clearNum(match[2]) / 100) + "%";
 });
 
 /* Value by percent part: 5% of what is 6 EUR */
 
-perEx(/\/per +([0-9]+)% of what is ([^a-z]+)/i, (match) => {
-  return (match[2].match(/\d+/)[0] / match[1]) * 100;
+perEx(/\/per +([0-9,.\s]+)%\s+of\s+what\s+is\s+(.+)/i, (match) => {
+  return (clearNum(match[2]) / clearNum(match[1])) * 100 + " " + curreNum(match[2]);
 });
