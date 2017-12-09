34cf4eaca7966088850be13741656e312741f45f
lenevro <barzarik@gmail.com>
2017-12-09 19:58:06 +0400

    Caching result, display changes and refactoring in currency

--- app/currency.js
+++ app/currency.js
@@ -1,83 +1,147 @@
-const request = require('request'),
-      bot = require('./bot');
+const bot = require('./bot'),
+      fetch = require('node-fetch'),
+      fx = require('money'),
+      moment = require('moment-timezone'),
+      cronNote = require('cron').CronJob;
+      
+let fxLatest,
+    fxYester;
 
-/* Currency function */
+function round(data) {
+  return Math.round(data * 1000) / 1000;
+}
 
-bot.onText(/\/cc ([0-9]+) ([a-z]+) to ([a-z]+) *(.+[^a-z]+)*/i, (msg, match) => {
-    const userId = msg.from.id;
-
-    let getDate = match[4] ? match[4].replace(/for /, "") : 'latest',
-        result,
-        formResult;
-
-    request('http://api.fixer.io/' + getDate + '?base=' + match[2].toUpperCase(), (error, response, body) => {
-      let resVal;
-
-      try {
-        resVal = JSON.parse(body);
-
-        if (resVal.error || !resVal.rates[match[3].toUpperCase()]) {
-          throw new SyntaxError("Incorrect value");
-        } else if (new Date() - Date.parse(getDate) < 0) {
-          throw new SyntaxError("Incorrect date");
-        }
-      } catch (e) {
-        bot.sendMessage(userId, e.message);
-
-        return;
+function getDataCurrency() {
+  fetch('https://api.fixer.io/latest')
+    .then((resp) => {
+        return resp.json();
       }
+    )
+    .then((data) => {
+        fxLatest = data;
+        fxLatest.rates['EUR'] = 1;
 
-      result = match[1] * resVal.rates[match[3].toUpperCase()];
-      formResult = match[1] + " " + match[2].toUpperCase() + " = " + Math.round(result * 100) / 100 + " " + match[3].toUpperCase() +
-                   (getDate != "latest" ? "\n(Rate for " + getDate + ")" : "");
+        console.log(`Update latest rates! ${moment().format('h:mm:ss a')}`);
 
-      bot.sendMessage(userId, formResult);
-    });
+        return moment(data.date).subtract(1, 'day').format('YYYY-MM-DD');
+      }
+    )
+    .then((date) => {
+        fetch('https://api.fixer.io/' + date)
+          .then((resp) => {
+              return resp.json();
+            }
+          )
+          .then((data) => {
+            fxYester = data;
+            fxYester.rates['EUR'] = 1;
+
+            console.log(`Update yester rates! ${moment().format('h:mm:ss a')}`);
+          })
+      }
+    );
+}
+
+getDataCurrency();
+
+/* Set Cron */
+
+new cronNote({
+  cronTime: '00 59 * * * *',
+  onTick() {
+    getDataCurrency();
+  },
+  start: true,
+  timeZone: 'Europe/Brussels'
 });
 
+/* Bot Msg */
+
 bot.onText(/\/cc ([a-z]+) *(.+[^a-z]+)*/i, (msg, match) => {
-    const userId = msg.from.id;
-				
-    let getDate = match[2] ? match[2].replace(/for /, "") : 'latest',
-        result;
+  const userId = msg.from.id,
+        date = match[2] ? match[2].replace(/for /, '') : 'latest';
+      
+  let getData;
 
-    request('http://api.fixer.io/' + getDate + '?base=' + match[1].toUpperCase(), (error, response, body) => {
-      let resVal,
-          topVal;
-
-      try {
-        resVal = JSON.parse(body);
-
-        if (resVal.error) {
-          throw new SyntaxError("Incorrect value");
-        } else if (new Date() - Date.parse(getDate) < 0) {
-          throw new SyntaxError("Incorrect date");
+  if (date != 'latest') {
+    fetch('https://api.fixer.io/' + date)
+      .then((resp) => {
+          return resp.json();
         }
-      } catch (e) {
-        bot.sendMessage(userId, e.message);
+      )
+      .then((data) => {
+          fx.rates = data.rates;
+          fx.rates['EUR'] = 1;
+        }
+      )
+      .then(() => {
+          getData = key => round(fx(1).from(match[1].toUpperCase()).to(key));
 
-        return;
-      }
+          sendMsg();
+        }
+      );
+  } else {
+    getData = (key) => {
+      fx.rates = fxYester.rates;
+      const yester = round(fx(1).from(match[1].toUpperCase()).to(key));
 
-      function round(key) {
-        return Math.round(resVal.rates[key] * 100) / 100;
-      }
+      fx.rates = fxLatest.rates;
+      const latest = round(fx(1).from(match[1].toUpperCase()).to(key));
 
-      topVal = [
-        "🇪🇺 EUR: " + round("EUR"),
-        "🇺🇸 USD: " + round("USD"),
-        "🇷🇺 RUB: " + round("RUB")
-      ];
+      return `${latest} [` + ((latest - yester) > 0 ? '+' + round(latest - yester) : round(latest - yester)) + ']';
+    }
 
-      topVal.forEach((item, i) => {
-        if (~item.indexOf(match[1].toUpperCase())) topVal.splice(i, 1);
-      });
+    sendMsg();
+  }
 
-      result = "<b>Top " + match[1].toUpperCase() + " Exchange Rates</b>\n\n" + topVal.join("\n") +
-               (getDate != "latest" ? "\n\n(Rates for " + getDate + ")" : "");
+  function sendMsg() {
+    const topVal = [
+      `🇪🇺 EUR: ${getData('EUR')}`,
+      `🇺🇸 USD: ${getData('USD')}`,
+      `🇷🇺 RUB: ${getData('RUB')}`
+    ];
 
-      bot.sendMessage(userId, result, {
-        parse_mode: "HTML"
-      });
+    topVal.forEach((item, i) => {
+      if (~item.indexOf(match[1].toUpperCase())) topVal.splice(i, 1);
     });
+
+    const result = `<b>Top ${match[1].toUpperCase()} Exchange Rates</b>\n\n` + topVal.join('\n') +
+                   (date != 'latest' ? `\n\n(Rates for ${date})` : `\n\n(Last update: ${fxLatest.date})`);
+
+    bot.sendMessage(userId, result, {
+      parse_mode: 'HTML'
+    });
+  }
+});
+
+bot.onText(/\/cc ([0-9]+) ([a-z]+) to ([a-z]+) *(.+[^a-z]+)*/i, (msg, match) => {
+  const userId = msg.from.id,
+        date = match[4] ? match[4].replace(/for /, '') : 'latest';
+
+  if (date != 'latest') {
+    fetch('https://api.fixer.io/' + date)
+      .then((resp) => {
+          return resp.json();
+        }
+      )
+      .then((data) => {
+          fx.rates = data.rates;
+          fx.rates['EUR'] = 1;
+        }
+      )
+      .then(() => sendMsg());
+  } else {
+    fx.rates = fxLatest.rates;
+
+    sendMsg();
+  }
+
+  function sendMsg() {
+    const result = fx(match[1]).from(match[2].toUpperCase()).to(match[3].toUpperCase());
+
+    const buildMsg = `${match[1]} ${match[2].toUpperCase()} = ${round(result)} ${match[3].toUpperCase()}` +
+                     (date != 'latest' ? `\n(Rate for ${date})` : `\n(Last update: ${fxLatest.date})`);
+
+    bot.sendMessage(userId, buildMsg);
+  }
 });
