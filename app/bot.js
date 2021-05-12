93faa4fa2042553a9bcd1320caaeb444cdedbff5
leusrox <leusrox@gmail.com>
2021-05-13 02:05:28 +0300

    Remove socks5 client

--- app/bot.js
+++ app/bot.js
@@ -13,22 +13,9 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   require('dotenv').config();
 
-  const agent = require('socks5-https-client/lib/Agent');
-
   token = process.env.BOT_TOKEN_LOCAL;
 
-  bot = new botApi(token, { 
-    polling: true,
-    request: {
-      agentClass: agent,
-      agentOptions: {
-        socksHost: process.env.PROXY_SOCKS5_HOST,
-        socksPort: parseInt(process.env.PROXY_SOCKS5_PORT),
-        // socksUsername: process.env.PROXY_SOCKS5_USERNAME,
-        // socksPassword: process.env.PROXY_SOCKS5_PASSWORD
-      }
-    }
-  });
+  bot = new botApi(token, { polling: true });
   bot.setWebHook();
 }
 
